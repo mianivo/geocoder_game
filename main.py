20a806c48f16b78c1d79517a542e4e723e128117
msib <msibiryaev@yandex.ru>
2021-04-29 13:47:30 +0500

    db

--- main.py
+++ main.py
@@ -296,7 +296,7 @@ def game():
                 session["gamenum"] = gamenum
                 session["gamescore"] = gamescore
                 return render_template('roundresult.html', dist=dist, score=score,
-                                       x1=coords[1], y1=coords[0], x2=x, y2=y,
+                                       y1=coords[1], x1=coords[0], y2=x, x2=y,
                                        gamescore=gamescore, gamenum=gamenum)
             else:
                 # если игра закончилась
